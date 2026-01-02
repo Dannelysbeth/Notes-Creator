9275a0190c1d542a69a5b15e8522cc0b7ac25582
Dannelysbeth <danielaf@onet.pl>
2026-01-02 11:21:45 +0100

    reformat code

--- file-handling/Note.rb
+++ file-handling/Note.rb
@@ -3,22 +3,22 @@ require 'date'
 require 'fileutils'
 
 class Note
-  attr_accessor :fileName, :content, :modificationTime
-  attr_reader :creationTime
+  attr_accessor :file_name, :content, :modification_time
+  attr_reader :creation_time
 
   def initialize(content = '', directory = '.', fileName = nil)
     FileUtils.mkdir_p(directory)
 
     base_name = (fileName || Date.today.to_s) + ".md"
-    @fileName = File.join(directory, base_name)
+    @file_name = File.join(directory, base_name)
 
-    @creationTime = Time.now
-    @modificationTime = @creationTime
+    @creation_time = Time.now
+    @modification_time = @creation_time
     @content = "Date: #{Date.today} \n #{content}"
   end
 
   def save
-    file = File.open(@fileName, "w")
+    file = File.open(@file_name, "w")
     file.write(@content)
     file.close
   end
